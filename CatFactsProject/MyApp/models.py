50de6ab90d8f0f868f9df2e398ea5299132b0a1b
Gustavo <Gholley42@gmail.com>
2022-12-22 19:02:12 -0300

    Carga de templates + inicio de creacion de sistema login

--- CatFactsProject/MyApp/models.py
+++ CatFactsProject/MyApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
