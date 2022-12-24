6b06028e6564b919650cc769e8711fb3b11b97b4
Gustavo <Gholley42@gmail.com>
2022-12-24 00:56:11 -0300

    Conexion API

--- CatFactsProject/MyApp/views.py
+++ CatFactsProject/MyApp/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib.auth.decorators import login_required
+import requests
 from django.contrib import messages
 from django.http import HttpResponse
 from .models import Profile
 
 # Create your views here.
 
-@login_required(login_url='signin')
+#@login_required(login_url='signin')
 def index(request):
-    return render(request, 'index.html')
+    response = requests.get('https://catfact.ninja/fact')
+    facts = response.json()
+    return render(request, 'index.html', {'facts': facts})
 
 def signup(request):
     if request.method =='POST':
@@ -40,7 +43,7 @@ def signin(request):
         return render(request, 'signin.html')
 
 
-@login_required(login_url='signin')
+#@login_required(login_url='signin')
 def logout(request):
     auth.logout(request)
     return redirect('signin')
